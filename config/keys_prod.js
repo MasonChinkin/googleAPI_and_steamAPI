3843bda43c1b319ab23081c3d945623f0b924c25
Mason Chinkin <mason.chinkin@gmail.com>
2019-05-14 17:19:49 -0700

    Implement Google Sheets API on Express backend

--- config/keys_prod.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  sheets: process.env.SHEETS,
-  clientID: process.env.CLIENT_ID,
-  clientSecret: process.env.CLIENT_SECRET
-};
--- routes/members.js
+++ routes/members.js
@@ -4,66 +4,34 @@ const {
   google
 } = require('googleapis');
 const sheets = google.sheets('v4');
-const creds = require('../config/keys_dev')
+const privateKey = require('../bin/privateKey.json')
 
 router.get('/', function (req, res, next) {
-  authorize((authClient) => {
-    const request = {
-      spreadsheetId: '1AuE8xWqvRqyM1FRWJPjhVXRuHfUcal5bgHx6jkcyA7M',
-      range: 'A1:B3',
-      auth: authClient,
-    };
 
-    sheets.spreadsheets.values.get(request, (err, response) => {
-      if (err) {
-        console.error(err);
-        return;
-      }
-
-      console.log(response)
-    })
+  // configure a JWT auth client
+  let jwtClient = new google.auth.JWT(
+    privateKey.client_email,
+    null,
+    privateKey.private_key,
+    ['https://www.googleapis.com/auth/spreadsheets.readonly']);
+  //authenticate request
+  jwtClient.authorize(function (err, tokens) {
+    (err) ? console.log(err): console.log("Successfully connected!")
   });
 
+  let request = {
+    spreadsheetId: '1AuE8xWqvRqyM1FRWJPjhVXRuHfUcal5bgHx6jkcyA7M', // TODO: Update placeholder value.
+    range: 'sheet1', // TODO: Update placeholder value.
+    auth: jwtClient,
+  };
+
+  sheets.spreadsheets.values.get(request, function (err, response) {
+    if (err) {
+      console.error(err);
+      return;
+    }
+    res.status(200).json(response.data.values)
+  })
 })
-
-
-function authorize(callback) {
-  // TODO: Change placeholder below to generate authentication credentials. See
-  // https://developers.google.com/sheets/quickstart/nodejs#step_3_set_up_the_sample
-  //
-  // Authorize using one of the following scopes:
-  //   'https://www.googleapis.com/auth/drive'
-  //   'https://www.googleapis.com/auth/drive.file'
-  //   'https://www.googleapis.com/auth/drive.readonly'
-  //   'https://www.googleapis.com/auth/spreadsheets'
-  //   'https://www.googleapis.com/auth/spreadsheets.readonly'
-  const authClient = null;
-
-  const oauth2Client = new google.auth.OAuth2(
-    YOUR_CLIENT_ID,
-    YOUR_CLIENT_SECRET,
-    YOUR_REDIRECT_URL
-  );
-
-  // generate a url that asks permissions for Blogger and Google Calendar scopes
-  const scopes = [
-    'https://www.googleapis.com/auth/blogger',
-    'https://www.googleapis.com/auth/calendar'
-  ];
-
-  const url = oauth2Client.generateAuthUrl({
-    // 'online' (default) or 'offline' (gets refresh_token)
-    access_type: 'offline',
-
-    // If you only need one scope you can pass it as a string
-    scope: scopes
-  });
-
-  if (authClient == null) {
-    console.log('authentication failed');
-    return;
-  }
-  callback(authClient);
-}
 
 module.exports = router;
