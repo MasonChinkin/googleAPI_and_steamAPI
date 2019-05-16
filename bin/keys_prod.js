f7ba9f22688a7a239411cda11bc0f159b480017c
Mason Chinkin <mason.chinkin@gmail.com>
2019-05-16 16:01:22 -0700

    Third method to handle keys securely

---
+++ bin/keys_prod.js
@@ -0,0 +1,4 @@
+module.exports = {
+  client_email: process.env.GOOGLEAPI_CREDENTIALS_CLIENT_EMAIL,
+  private_key: process.env.GOOGLEAPI_CREDENTIALS_PRIVATE_KEY
+};
--- routes/members.js
+++ routes/members.js
@@ -4,25 +4,15 @@ const {
   google
 } = require('googleapis');
 const sheets = google.sheets('v4');
-
-let clientEmail, privateKey;
-if (process.env.NODE_ENV === 'production') {
-  clientEmail = process.env.GOOGLEAPI_CREDENTIALS_CLIENT_EMAIL
-  privateKey = process.env.GOOGLEAPI_CREDENTIALS_PRIVATE_KEY
-} else {
-  const privateKeyJSON = require('../bin/privateKey.json')
-  clientEmail = privateKeyJSON.client_email
-  privateKey = privateKeyJSON.private_key
-}
-
+const keys = require('../bin/keys')
 
 router.get('/', function (req, res, next) {
 
   // configure a JWT auth client
   let jwtClient = new google.auth.JWT(
-    clientEmail,
+    keys.client_email,
     null,
-    privateKey,
+    keys.private_key,
     ['https://www.googleapis.com/auth/spreadsheets.readonly']);
   //authenticate request
   jwtClient.authorize(function (err, tokens) {
