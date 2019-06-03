c308f4844cace5c2fb0eab808c1d51f9cacb2903
Mason Chinkin <mason.chinkin@gmail.com>
2019-06-03 13:17:18 -0700

    react-iframe with gcal for events page

--- frontend/src/components/events/Events.jsx
+++ frontend/src/components/events/Events.jsx
@@ -1,11 +1,16 @@
 import React from 'react';
+import Iframe from 'react-iframe'
 
 const Events = () => {
   return (
     <main className="events">
       <section>
         <h2 className="events-title">UPCOMING EVENTS</h2>
-        <div className="calendar-placeholder backdrop">placeholder</div>
+        <Iframe
+          className="calendar"
+          url="https://calendar.google.com/calendar/embed?&amp;wkst=1&amp;bgcolor=%23ffffff&amp;ctz=America%2FLos_Angeles&amp;src=Y2hpbmtpbm1hQGdtYWlsLmNvbQ&amp;src=bWFzb24uY2hpbmtpbkBnbWFpbC5jb20&amp;src=ZW4udXNhI2hvbGlkYXlAZ3JvdXAudi5jYWxlbmRhci5nb29nbGUuY29t&amp;color=%233F51B5&amp;color=%237986CB&amp;color=%23009688&amp;showCalendars=0&amp;showTabs=0&amp;showPrint=0&amp;showNav=1&amp;showTitle=0"
+        />
+
       </section>
     </main>
   );
